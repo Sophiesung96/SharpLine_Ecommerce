d41b25d38e2c17884060f90db09b34eceb6c800f
webtaiwandev1306 <92771270+webtaiwandev1306@users.noreply.github.com>
2024-01-23 12:38:09 +1100

    1 !

--- src/test/java/com/example/demo01/src/Controller/UserRestControllerTest.java
+++ src/test/java/com/example/demo01/src/Controller/UserRestControllerTest.java
@@ -1,16 +1,28 @@
 package com.example.demo01.src.Controller;
 
+import com.example.demo01.src.Pojo.Customer;
 import com.example.demo01.src.Pojo.Users;
+import com.example.demo01.src.Service.CustomerService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import javax.annotation.PostConstruct;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
@@ -22,23 +34,18 @@ class UserRestControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+
+
+
     @Test
     public void getById() throws Exception {
         Users user=new Users();
         user.setEmail("haha@gmail.com");
         String data=objectMapper.writeValueAsString(user);
-        RequestBuilder requestBuilder= MockMvcRequestBuilders
-                .post("/checkEmail")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(data);
-      mockMvc.perform(requestBuilder)
-                .andDo(print());
-
-
-
-
-
+      mockMvc.perform(post("/checkEmail")
+                      .content(data)
+                      .contentType(MediaType.APPLICATION_JSON))
+                      .andDo(print());
     }
-
 
 }
