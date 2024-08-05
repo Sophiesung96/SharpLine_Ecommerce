522dcb06c58a7c597baff82507140a7d2933ec01
webtaiwandev1306 <92771270+webtaiwandev1306@users.noreply.github.com>
2024-08-05 18:40:36 +1000

    moved the WebConfig file to SecurityConfig folder

--- src/main/java/com/example/demo01/src/Configuration/SecurityConfig/WebConfig.java
+++ src/main/java/com/example/demo01/src/Configuration/SecurityConfig/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo01.src.Configuration;
+package com.example.demo01.src.Configuration.SecurityConfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
